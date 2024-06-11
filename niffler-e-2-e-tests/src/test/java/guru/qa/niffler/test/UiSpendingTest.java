ddb3ee7f0a9739e772909f51de3d206a9d2686b7
Yevgeny Demidov <Yevgeny.Demidov@nedra.digital>
2024-06-11 09:04:20 +0300

    add addNewCategory

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/UiSpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/UiSpendingTest.java
@@ -6,6 +6,7 @@ import guru.qa.niffler.jupiter.extension.DbCreateUserExtension;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.AuthorizationPage;
 import guru.qa.niffler.page.MainPage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -19,11 +20,22 @@ import static com.codeborne.selenide.Selenide.open;
 public class UiSpendingTest {
 	MainPage mainPage = new MainPage();
 
+	@BeforeEach
+	public void login(UserJson userJson) {
+		open(MainPage.url, AuthorizationPage.class).clickLoginButton().login(userJson);
+	}
+
 	@Test
 	@TestUser
 	public void selectDataSpend(UserJson userJson) {
-		open(MainPage.url, AuthorizationPage.class).clickLoginButton().login(userJson);
 		Date date = new GregorianCalendar(2024, Calendar.FEBRUARY, 1).getTime();
 		mainPage.setSpendDate(date).checkSpendDate(date);
+	}
+
+	@Test
+	@TestUser
+	public void addNewCategory() {
+		String category = "Обучение";
+		mainPage.
 	}
 }
